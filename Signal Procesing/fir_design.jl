dc45109597369f15672ae954a3d9e4b5ea5bb2e5
Clemens Novak <clemens@familie-novak.net>
2014-12-13 17:29:37 +0100

    added another function for fir design using multi-dispatch

--- Signal Procesing/fir_design.jl
+++ Signal Procesing/fir_design.jl
@@ -2,7 +2,7 @@ using Winston
 include("dsp_stuff.jl")
 
 
-function fir_window_design(L, passbands, win_fct)
+function fir_window_design(L :: Int64, passbands :: Array{Float64,2}, win_fct :: Function)
 
 	m = [0:L-1] - (L-1)/2
 
@@ -23,3 +23,36 @@ function fir_window_design(L, passbands, win_fct)
 
 
 end
+
+
+
+type Passband
+	attn :: Float64
+	start :: Float64
+	stop :: Float64
+end
+
+
+function fir_window_design(L :: Int64, passbands :: Array{Passband,1}, win_fct :: Function)
+
+	m = [0:L-1] - (L-1)/2
+
+	npbs = length(passbands)
+
+	h = zeros(L)
+
+	for i in 1:npbs
+		
+		pb = passbands[i]
+		temp = pb.attn * pb.stop / pi * sinc(pb.stop*m/pi)
+		h = h + temp
+		temp = pb.attn * pb.start / pi * sinc(pb.start*m/pi)
+		h = h - temp
+	end
+
+	win = win_fct(L)
+	hwin = h .* win
+
+end
+
+
