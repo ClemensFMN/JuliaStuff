945fc3b9ecba626fa210316088021249d1425970
NOVAK Clemens <Clemens.NOVAK@frequentis.com>
2016-07-11 13:44:56 +0200

    added case when only pressing of different buttons is allowed

--- stochastic/elevator_problem.jl
+++ stochastic/elevator_problem.jl
@@ -4,6 +4,7 @@
 # the airplane thing is further elaborated here:
 # http://math.stackexchange.com/questions/5595/taking-seats-on-a-plane
 
+using StatsBase
 
 N = round(Int, 1e6)
 
@@ -12,18 +13,20 @@ M = 13
 buttons = 1:M
 
 # number of keypresses
-L = 5
+L = 3
 
 success = 0
 
 for i in 1:N
     # we take a sample of the L button presses - note that we allow pressing the same button more than once
-    sample = rand(buttons, L)
-    #println(sample)
+    # smpl = sample(buttons, L, replace=true)
+    # we take a sample of the L button presses - note that we do NOT allow pressing the same button more than once
+    smpl = sample(buttons, L, replace=false)
+    #println(smpl)
     # L consecutive floors -> difference between button presses (in SORTED order) is all one
-    trnsfmd = diff(sort(sample))
+    trnsfmd = diff(sort(smpl))
     if(all(x->x==1, trnsfmd))
-	#println(sample)
+	#println(smpl)
 	success = success + 1
     end
 end
@@ -33,5 +36,9 @@ println(success / N)
 # example for L=3: (1,2,3), (2,3,4)... (M-2,M-1,M)
 # every block can be re-arranged in L! different ways: (1,2,3), (1,3,2). (2,1,3), (2,3,1), (3,1,2), (3,2,1)
 # therefore we have (M-L+1) x L! good cases
-# and in total we have L places into which we can place M different elements -> M^L cases
+# allowing pressing the same button more than one: in total we have L places into which we can place M different elements -> M^L cases
 println(factorial(L)*(M-L+1)/M^L)
+
+# NOT allowing pressing the same button more than once: in total we have L places into which we can place M different elements -> M x M-1 x M-2 ... M-L+1 cases
+println(factorial(L)*(M-L+1)/(13*12*11))
+
