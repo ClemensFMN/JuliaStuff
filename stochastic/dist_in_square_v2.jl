b2fadb595775b0301822592b1492b7dfb9990ea2
Clemens Novak <clemens@familie-novak.net>
2016-12-01 15:27:22 +0100

    latets additions

--- stochastic/dist_in_square_v2.jl
+++ stochastic/dist_in_square_v2.jl
@@ -1,13 +1,14 @@
 
 
 runs = int(1e6)
-dim = 10
+dim = 2
+displacement = 5
 dist = zeros(runs)
 
 for r in 1:runs
 
     p1 = rand(dim)
-    p2 = rand(dim)
+    p2 = displacement + rand(dim)
 
     dist[r] = norm(p1-p2)
 
