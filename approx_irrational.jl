1acf16e0ecf117490cdef15c7897605d1bc04a26
Clemens Novak <clemens@familie-novak.net>
2017-08-21 13:40:32 +0200

    rational approximation of irrationals

---
+++ approx_irrational.jl
@@ -0,0 +1,24 @@
+# find rational approximations for an irrational number (pi in this case)
+# we consider abs( target - n/d) as error function
+target = e # pi
+
+# we subsequently increase d (up to DMax)
+DMax = 1000
+
+d = 1
+# in parallel we keep track of the approximation error seen so far
+err_so_far = 1
+
+
+while(d < DMax)
+   # obtain a new approximation
+   n = round(target * d)
+   # calculate approximation error
+   approx_err = abs(n/d - target)
+   # only print the new approx if it is better than what we have seen so far
+   if(approx_err < err_so_far)
+      println(n, "...", d, "...", approx_err)
+      err_so_far = approx_err
+   end
+   d = d + 1
+end
