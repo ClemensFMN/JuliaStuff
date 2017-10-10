70cd430309561704cb389a85644e6521d158db08
Clemens Novak <clemens@familie-novak.net>
2017-10-10 14:07:46 +0200

    clean up of EC group stuff

--- elliptic_curve_group.jl
+++ elliptic_curve_group.jl
@@ -1,26 +1,44 @@
-immutable ECSystem
+mutable struct ECSystem
 # representing a curve y^2 \equiv x^3 + ax + b \bmod p
     p :: Integer
     a :: Integer
     b :: Integer
 end
 
-
-immutable ECPoint
+# a point on an elliptic curve
+mutable struct ECPoint
     s :: ECSystem
     x :: Integer
     y :: Integer
 end
+
+immutable ECId
+end
+
+# a group element on an elliptic curve can be either a point or the identity element
+ECGroupElement = Union{ECPoint, ECId}
 
 
 # interestingly, there seems to be some kind of short function definition...
 # neverthless, we need to extend the Base function - to avoid ambiguities, we need to say (==)
 Base.:(==)(p1::ECPoint, p2::ECPoint) = (p1.x == p2.x) && (p1.y == p2.y)
 
+function Base.:(==)(p1::ECGroupElement, p2::ECGroupElement)
+    if(p1==ECId & p2 == ECId) true
+    elseif(p1 == ECPoint & p2 == ECPoint)
+        p1 == p2
+    else
+        false
+    end
+end
+
+
 
 # overload the + operator to work with points on elliptic curves
 function Base.:+(p1::ECPoint, p2::ECPoint)
-    if(p1.s.p != p2.s.p) error("same p required")
+    if(p1.s.p != p2.s.p)
+        error("same p required")
+    end
     p = p1.s.p
     # TODO check that both p1.s.p and p2.s.p are equal
     if(p1==p2)
@@ -29,8 +47,11 @@ function Base.:+(p1::ECPoint, p2::ECPoint)
     else
         # TODO what happens when p2.x. and p1.x. are equal?? then return the identity element, but how do we represent that?
         # -> the type of ECPoint is not finished...
-        s = (p2.y - p1.y) * invmod(p2.x - p1.x, p)
-        s = mod(s, p)
+        if(p1.x == p2.x) return ECId()
+        else
+            s = (p2.y - p1.y) * invmod(p2.x - p1.x, p)
+            s = mod(s, p)
+        end
     end
 
     tempx = s^2 - p1.x - p2.x
@@ -38,11 +59,36 @@ function Base.:+(p1::ECPoint, p2::ECPoint)
     ECPoint(p1.s, mod(tempx, p), mod(tempy, p))
 end
 
+
+
+#function Base.:+(p1::ECGroupElement, p2::ECGroupElement)
+#    if(p1 == ECId) p2
+#    elseif(p2 == ECId) p1
+#    else
+#        p1 + p2
+#    end
+#end
+
+Base.:+(p1::ECPoint, p2::ECId) = p1
+Base.:+(p1::ECId, p2::ECPoint) = p2
+Base.:+(p1::ECId, p2::ECId) = ECId()
+
+
 # based on Understanding Cryptography, Section 9.2
 s = ECSystem(17, 2, 2)
 
+
+pId = ECId()
+
 # a starting point
 p1 = ECPoint(s, 5, 1)
+
+
+# some testing
+println(pId + pId)
+println(pId + p1)
+println(p1 + pId)
+
 
 pnew = p1
 
