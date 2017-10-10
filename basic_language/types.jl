ef5eca1551dccc1c5f647772b324ffb9eb340c1e
Clemens Novak <clemens@familie-novak.net>
2017-10-10 14:11:15 +0200

    type unions

--- basic_language/types.jl
+++ basic_language/types.jl
@@ -11,3 +11,35 @@ p1 = Point(2,3)
 p2 = Point(4,5)
 
 println(dist(p1, p2))
+
+# some kind of "indicator type"
+immutable Id
+end
+
+# defining a union
+PointOrId = Union{Point, Id}
+
+# some stupid function returning our union type
+function f1(x)
+    if(x == 0) Id()
+    else Point(x,x)
+    end
+end
+
+println(f1(0))
+println(f1(1))
+
+
+function f2(x::PointOrId)
+    if(typeof(x) == Point)
+        return(x.x + x.y)
+    else
+        return "Id"
+    end
+end
+
+
+pID = Id()
+println(f2(p1))
+println(f2(pID))
+
