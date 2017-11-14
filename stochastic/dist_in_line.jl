4d7393cc82b1e35f7eb0b41485fbac07126e2ffd
Clemens Novak <clemens@familie-novak.net>
2017-11-14 14:51:11 +0100

    modified / corrected

--- stochastic/dist_in_line.jl
+++ stochastic/dist_in_line.jl
@@ -1,26 +1,30 @@
 # using Winston;
 
+# distance between two random points on two lines
+# each line has a length of B and the spacing between the lines is L
+
 runs = round(Int, 1e7)
 
-L = 2
+# the spacing of the lines
+L = 1
+# the length of the line
+B = 1
 
 dist = zeros(runs)
-dist_sq = zeros(runs)
+dist2 = zeros(runs)
 
 for r in 1:runs
-
-    x1 = rand()
-    x2 = L + rand()
-    dist[r] = x2 - x1
-    dist_sq[r] = (x2 - x1)^2
-
+    x1 = B*rand() # pick a random point on the lower line
+    x2 = B*rand() # pick a random point on the upper line
+    d = (x2 - x1)^2 + L^2 # claculate the squared distance
+    dist2[r] = d
+    dist[r] = sqrt(d)
 end
 
 Ed = mean(dist)
-Ed2 = mean(dist_sq)
+Ed2 = mean(dist2)
 
-println(Ed)
-println(Ed^2)
-println(Ed2)
-# For fixed x1 (equal 0), the difference is independent of L! It is 1/12
-println(Ed2 - Ed^2)
+println("mean squared distance: ", Ed2)
+println("mean squared distance, analytical result: ", B^2/6 + L^2)
+
+println("mean distance: ", Ed)
