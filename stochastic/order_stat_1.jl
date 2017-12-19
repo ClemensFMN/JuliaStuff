2bb9568751198f8fa892444250589cd995994e10
Clemens Novak <clemens@familie-novak.net>
2017-12-19 10:20:53 +0100

    order statistics, cont'd

--- stochastic/order_stat_1.jl
+++ stochastic/order_stat_1.jl
@@ -9,22 +9,24 @@ s2 = 1.5
 #p = Normal(0,sqrt(s2))
 p = Uniform()
 
-k = 5
+k = 10
 
 N = 500000
-edges = linspace(-2,2, 50)
+edges = linspace(-2,2,50)
 
 x = rand(p, k, N)
 
 # and take the maximum of k samples; i.e. the k-th order statistics
 res = maximum(x, 1)
-hres = fit(Histogram, res[1,:], edges)
+hres = fit(Histogram, res[1,:], edges, closed=:right)
 
-t = linspace(-5,5)
-y = 1/sqrt(2*pi*s2)*exp.(-t.^2/(2*s2))
+# t = linspace(-5,5)
+t = linspace(0,1)
+# y = 1/sqrt(2*pi*s2)*exp.(-t.^2/(2*s2))
+y = k*t.^(k-1)
 
-hx = fit(Histogram, x[1,:], edges)
+#hx = fit(Histogram, x[1,:], edges, closed=:right)
 # we need to normalize the shit by taking into account the width of the bins
-plot(edges, hx.weights/N/(t[2]-t[1]))
-#plot!(t, y)
-plot!(edges, hres.weights/N/(t[2]-t[1]))
+# plot(edges, hx.weights/N/(t[2]-t[1]))
+plot(t, y)
+plot!(edges, hres.weights/N/(edges[2]-edges[1]))
