b8a6e0726f4261edb5da2852521f627c80ff4ef5
Clemens Novak <clemens@familie-novak.net>
2017-12-21 16:06:40 +0100

    order stats, general case

--- stochastic/order_stat_1.jl
+++ stochastic/order_stat_1.jl
@@ -9,24 +9,28 @@ s2 = 1.5
 #p = Normal(0,sqrt(s2))
 p = Uniform()
 
-k = 10
+kmax = 10
+k = 1
 
 N = 500000
 edges = linspace(-2,2,50)
 
-x = rand(p, k, N)
+x = rand(p, kmax, N)
+
+# sort column-wise
+x = sort(x,1)
 
 # and take the maximum of k samples; i.e. the k-th order statistics
-res = maximum(x, 1)
-hres = fit(Histogram, res[1,:], edges, closed=:right)
+res = x[k,:]
+hres = fit(Histogram, res, edges, closed=:right)
 
 # t = linspace(-5,5)
-t = linspace(0,1)
+# t = linspace(0,1)
 # y = 1/sqrt(2*pi*s2)*exp.(-t.^2/(2*s2))
-y = k*t.^(k-1)
+# y = k*t.^(k-1)
 
 #hx = fit(Histogram, x[1,:], edges, closed=:right)
 # we need to normalize the shit by taking into account the width of the bins
 # plot(edges, hx.weights/N/(t[2]-t[1]))
-plot(t, y)
-plot!(edges, hres.weights/N/(edges[2]-edges[1]))
+#plot(t, y)
+plot(edges, hres.weights/N/(edges[2]-edges[1]))
--- stochastic/order_stat_2.jl
+++ stochastic/order_stat_2.jl
@@ -2,14 +2,17 @@ using Distributions
 using Plots
 plotly()
 
-N = 5
+N1 = 4
+N2 = 10
 
 d = Normal(0,2)
 
 x = linspace(-10,10,100)
 
 pdf_x = pdf.(d,x)
-pdf_max = N * cdf.(d,x).^(N-1) .* pdf.(d,x)
+pdf_max_1 = N1 * cdf.(d,x).^(N1-1) .* pdf.(d,x)
+pdf_max_2 = N2 * cdf.(d,x).^(N2-1) .* pdf.(d,x)
 
 plot(x,pdf_x)
-plot!(x,pdf_max)
+plot!(x,pdf_max_1)
+plot!(x,pdf_max_2)
