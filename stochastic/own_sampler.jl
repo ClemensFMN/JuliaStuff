f206600dd3d0fe9b4442490c3190f0ca19e297fd
Clemens Novak <clemens@familie-novak.net>
2018-12-04 16:41:06 +0100

    distribution.jl, custom sampler (disrete RVs)

--- stochastic/own_sampler.jl
+++ stochastic/own_sampler.jl
@@ -1,44 +1,36 @@
 # creating own samplers in the Distributions.jl framework...
 using Distributions
 
-# a simple univariate and discrete distribution
-type SimpleDist <: Sampleable{Univariate, Discrete}
+# a simple univariate and discrete distribution for a uniform distribution between 0 and par
+struct SimpleDist <: Sampleable{Univariate,Discrete}
     par::Int # takes one parameter
 end
 
-# and implements a sampling function...
+# implementing the sampling function
 function Distributions.rand(S::SimpleDist)
-    # this also works without providing the namespace
-    p = Distributions.DiscreteUniform(0,S.par)
-    # but this REQUIRES the namespace
-    val = Distributions.rand(p)
-    # like this one
-    #val = Base.rand(1:S.par)
-    return(val)
+  # setting up a uniform dist between 0 and par
+  p = Distributions.DiscreteUniform(0,S.par)
+  # and sampling from it...
+  val = Distributions.rand(p)
+  return(val)
 end
 
 
-p = SimpleDist(4)
+# instantiate such a SimpleDist
+maxVal = 4
+p = SimpleDist(maxVal)
 
+# print a couple of values for fun
 for i in 1:10
     println(rand(p))
 end
 
+# and draw N RVs from SimpleDist
+N = 100_000
 
-# something a bit more advanced: a multivariate discrete sampler
-# which does not yet work...
-type CorrDice <: Sampleable{Multivariate, Discrete}
+# and make a simple histogram
+vls = rand(p, N)
+for i in 0:maxVal
+  ind = findall(x->x==i, vls)
+  println(i, "...", length(ind)/N)
 end
-
-# length of each sample is two
-Distributions.length(s::CorrDice) = 2
-
-
-function Distributions._rand!{T<:Real}(s::CorrDice, x::AbstractVector{T})
-    x = [1,1]
-    return(x)
-end
-
-
-p = CorrDice()
-rand(p)
