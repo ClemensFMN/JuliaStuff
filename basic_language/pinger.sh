484cc3c8b10af8a9c686e7dd3fb34033c13db9da
Clemens Novak <clemens@familie-novak.net>
2019-06-17 15:52:03 +0200

    pinger, use google

--- basic_language/pinger.sh
+++ basic_language/pinger.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while :
 do
-  ping -c 1 192.168.1.1 | tail -1| awk -F '/' 'END {print $5}' >> test.txt
+  ping -c 1 www.google.at | tail -1| awk -F '/' 'END {print $5}' >> ping_result.txt
   sleep 2
 done
