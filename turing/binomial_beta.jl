6dee65717f10e1810772158a84f8be5db2201b53
Clemens Novak <clemens@familie-novak.net>
2019-06-27 15:12:29 +0200

    binomial dist with beta prior, cont'd

--- turing/binomial_beta.jl
+++ turing/binomial_beta.jl
@@ -2,8 +2,8 @@
 using Turing
 using StatsPlots
 
-alpha = 1
-beta = 2
+alpha = 0.4
+beta = 1
 n = 10
 p = 0.3
 
@@ -18,5 +18,10 @@ k = 2
 c = sample(my_model(k), HMC(10000, 0.1, 5))
 # c = sample(normal_demo(0.5), SMC(1000))
 
-#(k + alpha)/(n + alpha + beta)
+# calculating the thing myself, i come to option 1
+# parameter choice above causes big change between option 1 & 2
+# -> seems option 1 is correct :-)
+# option 1
+# (k + alpha)/(n + alpha + beta)
+# option 2
 # (k + alpha -1)/(n + alpha + beta - 2)
