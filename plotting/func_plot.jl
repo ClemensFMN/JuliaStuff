7e954f3a8f755f1b90ef5b57fe0522f6f378d371
Clemens Novak <clemens@familie-novak.net>
2019-07-02 14:24:19 +0200

    beta distribution

--- plotting/func_plot.jl
+++ plotting/func_plot.jl
@@ -1,4 +1,3 @@
-# using Winston
 using Plots
 plotly()
 
@@ -10,8 +9,36 @@ x = range(0, stop=4, length=100)
 
 # y = vol(x)
 
-y1 = x.^2
-y2 = sqrt.(x)
+# y1 = x.^2
+# y2 = sqrt.(x)
 
-plot(x, y1)#, linecolor=:red)#, yscale=:log10)
+# plot(x, y1)#, linecolor=:red)#, yscale=:log10)
 #plot!(x,y2, linecolor=:blue)
+
+
+
+x=0:0.01:1
+
+# y1 = x.^0.3 .* (1 .- x ).^0.8
+# y1 = y1 ./ sum(y1)
+
+# y2 = x.^0.8 .* (1 .- x ).^0.3
+# y2 = y2 ./ sum(y2)
+
+# y3 = x.^0.5 .* (1 .- x ).^0.5
+# y3 = y3 ./ sum(y3)
+
+y1 = x.^1.3 .* (1 .- x ).^2.5
+y1 = y1 ./ sum(y1)
+
+y2 = x.^0.3 .* (1 .- x ).^1.8
+y2 = y2 ./ sum(y2)
+
+#y3 = x.^0 .* (1 .- x ).^0
+#y3 = y3 ./ sum(y3)
+
+
+
+plot(x,y1)
+plot!(x,y2)
+#plot!(x,y3)
