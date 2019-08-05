ed2dc4901d95393bb62355de14f9ca6ff44a5375
Clemens Novak <clemens@familie-novak.net>
2019-08-05 18:01:52 +0200

    discrete time queue

--- SimJulia/simple_queue.jl
+++ SimJulia/simple_queue.jl
@@ -21,24 +21,27 @@ end
 sim = Simulation()
 server = Resource(sim, 1)
 
-N = 1000
+N = 5_000
 
 waiting_time = zeros(N)
 
-
-proc_duration = 0.5
-
-e = Exponential(0.5)
+lambda = 2
+mu = 5
+pincoming = Exponential(1 / lambda)
+pprocess = Exponential(1 / mu)
 prev_time = 0.0
 
 for i = 1:N
-    global prev_time = prev_time + rand(e) # global is required so that we define prev_time outside the for loop
+    global prev_time = prev_time + rand(pincoming) # global is required so that we define prev_time outside the for loop
     #@show prev_time
-    @process client(sim, i, server, prev_time, proc_duration, waiting_time)
+    @process client(sim, i, server, prev_time, rand(pprocess), waiting_time)
 end
 
 run(sim)
 
 # plot(waiting_time)
 # histogram(waiting_time, bins=50)
+
+println(mean(waiting_time))
+println(lambda / mu / (mu - lambda))
 
