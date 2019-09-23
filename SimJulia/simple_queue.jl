5f4f387447981d5045dedaf2366e74aeb69914b2
Clemens Novak <clemens@familie-novak.net>
2019-09-23 13:49:04 +0200

    some additions to simple M/M/1 queue

--- SimJulia/simple_queue.jl
+++ SimJulia/simple_queue.jl
@@ -9,12 +9,16 @@ plotly()
     #println(name, " arriving at ", now(env))
     start = now(env)
     @yield request(server)
-    stop = now(env)
-    waiting_time[name] = stop - start
+    # if we want the mean waiting time UNTIL service, we need to measure stop here
+    #stop = now(env)
+    #waiting_time[name] = stop - start
     #println(name, " starting processing at ", now(env))
     @yield timeout(sim, proc_duration)
     #println(name, " finished processing at ", now(env))
     @yield release(server)
+    # this is the whole time: waiting AND processing
+    stop = now(env)
+    waiting_time[name] = stop - start
 end
 
 
@@ -25,16 +29,20 @@ N = 5_000
 
 waiting_time = zeros(N)
 
-lambda = 2
+lambda = 0.8
 mu = 5
 pincoming = Exponential(1 / lambda)
-pprocess = Exponential(1 / mu)
+#pprocess = Exponential(1 / mu)
+pprocess = Uniform(0,0.1)
 prev_time = 0.0
 
 for i = 1:N
     global prev_time = prev_time + rand(pincoming) # global is required so that we define prev_time outside the for loop
     #@show prev_time
-    @process client(sim, i, server, prev_time, rand(pprocess), waiting_time)
+    # random processing time
+    # @process client(sim, i, server, prev_time, rand(pprocess), waiting_time)
+    # deterministic processing time
+    @process client(sim, i, server, prev_time, 1.0, waiting_time)
 end
 
 run(sim)
@@ -43,5 +51,7 @@ run(sim)
 # histogram(waiting_time, bins=50)
 
 println(mean(waiting_time))
-println(lambda / mu / (mu - lambda))
-
+#waiting time only
+#println(lambda / mu / (mu - lambda))
+#waiting AND processing time
+println(1/(mu - lambda))
