12a2f317038e8be69cab5c18a6b3cf5bfb9dd2e6
Clemens Novak <clemens@familie-novak.net>
2020-01-24 09:00:03 +0100

    quicksort

--- algorithms/quicksort.jl
+++ algorithms/quicksort.jl
@@ -39,11 +39,44 @@ function quicksort!(A,i=1,j=length(A))
     return A
 end
 
-lst =[5, 6, 1, 7, 3, 2, 4, 9, 8]
+# partition function according to Cormen
+function partition_cormen(A,p,r)
+    x = A[r]
+    @show x
+    i = p-1
+    for j=p:r-1
+        @show j
+        @show A
+        if A[j] <= x
+            println("smaller")
+            i = i+1
+            @show i,j
+            A[i], A[j] = A[j], A[i]
+        end
+    end
+    A[i+1], A[r] = A[r], A[i+1]
+    return i+1
+end
+
+function qsort_cormen(A,p,r)
+    if(p < r)
+        q = partition_cormen(A,p,r)
+        qsort_cormen(A,p,q-1)
+        qsort_cormen(A,q+1,r)
+    end
+end
+
+
+lst =[2,8,7,1,3,5,6,4]
+
+
 
 #res = quicksort!(lst)
 #println(res)
 
-res = quicksort_func(lst)
-println(res)
+#res = quicksort_func(lst)
+#println(res)
     
+# res = partition_cormen(lst, 1, length(lst))
+qsort_cormen(lst,1,length(lst))
+lst
