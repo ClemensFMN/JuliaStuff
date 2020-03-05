def55f8d4191beb173feed2a51ed8619ac09975d
Clemens Novak <clemens@familie-novak.net>
2020-03-05 11:54:20 +0100

    graph depth-first search

---
+++ algorithms/graph_dfs.jl
@@ -0,0 +1,103 @@
+using DataStructures
+
+# type holding the different node colors
+@enum NodeColor White Grey Black
+
+# we have a DFSData structure which the DFS algorithm fills
+mutable struct DFSData
+    color :: NodeColor
+    d :: Int # time set to grey
+    f :: Int # time to black
+    parent :: String # parent node
+end
+
+
+function dfs(G)
+    # BFS again, this time the BFSData struct is used
+    verts = keys(G)
+    vertices = Dict{String, DFSData}()
+    
+    for v in verts # init
+        vertices[v] = DFSData(White, 0, 0, "")
+    end
+
+    global time = 0 # the global time parameter is super ugly
+
+    # for the purpose of demo, start with s, then t
+    #dfs_visit(G, vertices, "s")
+    dfs_visit(G, vertices, "t")
+    # second demo, start with t
+    # dfs_visit(G, vertices, "t")
+
+#    the correct approach: start with some vertex & choose another one as long as unvicited vertices are there...
+#    for u in verts
+#        if(vertices[u].color == White)
+#            dfs_visit(G, vertices, u, time)
+#        end
+#    end
+    vertices
+end
+
+
+function dfs_visit(G, vertices, u)
+    @show u
+    global time
+    global time = time + 1
+    vertices[u].d = time
+    vertices[u].color = Grey
+
+    for v in G[u]
+        if(vertices[v].color == White) # not visited?
+            vertices[v].parent = u
+            dfs_visit(G, vertices, v)
+        end
+    end
+
+    vertices[u].color = Black
+    global time = time + 1
+    vertices[u].f = time
+end
+
+
+function vertices2Dot(vertices)
+    println("digraph BST {")
+    for (vert, data) in vertices
+        strt = data.parent
+        stp = vert
+        if(strt != "" && stp != "")
+            println("$strt -> $stp;")
+        end
+        attrs = string(stp, " [label=\"", stp, " (", data.d, ", ", data.f, ")\"]")
+        println(attrs)
+    end
+    println("}")
+end
+
+function G2Dot(G)
+    println("digraph BST {")
+    for (vert, verts) in G
+        for v in verts
+            println(vert, "->", v, ";")
+        end
+    end
+    println("}")        
+end
+
+
+# the example graph from Cormen et al, Fig. 22.5
+G = Dict([
+    # "s" => ["w", "z"], # option 1
+    "s" => [], # option 2
+    "t" => ["u", "v"],
+    "u" => ["t", "v"],
+    "v" => ["s", "w"],
+    "w" => ["x"],
+    "x" => ["z"],
+    "y" => [],
+    "z" => ["w", "y"]])
+
+res = dfs(G)
+println(res)
+
+
+vertices2Dot(res)
