e494da3548973ff0dfcdfb7e40673a814f22bdcf
Clemens Novak <clemens@familie-novak.net>
2020-03-24 11:31:20 +0100

    DFS, topological sort

--- algorithms/graph_dfs.jl
+++ algorithms/graph_dfs.jl
@@ -1,4 +1,4 @@
-using DataStructures
+# using DataStructures
 
 # type holding the different node colors
 @enum NodeColor White Grey Black
@@ -13,7 +13,7 @@ end
 
 
 function dfs(G)
-    # BFS again, this time the BFSData struct is used
+    # DFS again, this time the DFSData struct is used
     verts = keys(G)
     vertices = Dict{String, DFSData}()
     
@@ -25,9 +25,11 @@ function dfs(G)
 
     # for the purpose of demo, start with s, then t
     #dfs_visit(G, vertices, "s")
-    dfs_visit(G, vertices, "t")
     # second demo, start with t
     # dfs_visit(G, vertices, "t")
+    # topological sort
+    dfs_visit(G, vertices, "A")
+    dfs_visit(G, vertices, "C")
 
 #    the correct approach: start with some vertex & choose another one as long as unvicited vertices are there...
 #    for u in verts
@@ -85,19 +87,32 @@ end
 
 
 # the example graph from Cormen et al, Fig. 22.5
+#G = Dict([
+#    # "s" => ["w", "z"], # option 1
+#    "s" => [], # option 2
+#    "t" => ["u", "v"],
+#    "u" => ["t", "v"],
+#    "v" => ["s", "w"],
+#    "w" => ["x"],
+#    "x" => ["z"],
+#    "y" => [],
+#    "z" => ["w", "y"]])
+
+#res = dfs(G)
+#println(res)
+
+
+#vertices2Dot(res)
+
+# example for topological sort
 G = Dict([
-    # "s" => ["w", "z"], # option 1
-    "s" => [], # option 2
-    "t" => ["u", "v"],
-    "u" => ["t", "v"],
-    "v" => ["s", "w"],
-    "w" => ["x"],
-    "x" => ["z"],
-    "y" => [],
-    "z" => ["w", "y"]])
+    "A" => ["B"],
+    "C" => ["B"],
+    "B" => ["D", "E"],
+    "E" => ["F"],
+    "F" => ["D"],
+    "D" => []])
 
 res = dfs(G)
 println(res)
 
-
-vertices2Dot(res)
