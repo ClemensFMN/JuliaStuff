7852cdd72883d072f10c93874f7c5454d763f925
Clemens Novak <clemens@familie-novak.net>
2020-04-09 12:21:13 +0200

    simplify...

--- algorithms/bellman_ford.jl
+++ algorithms/bellman_ford.jl
@@ -1,15 +1,3 @@
-
-
-
-#add_edge!(g,1,2)
-#add_edge!(g,1,3)
-#add_edge!(g,1,4)
-#add_edge!(g,2,3)
-#add_edge!(g,3,4)
-#add_edge!(g,3,5)
-#add_edge!(g,3,6)
-#add_edge!(g,5,6)
-
 mutable struct Vertex
     ID :: Int # vertex id
     d :: Int # upper bound for path length
@@ -20,14 +8,15 @@ Vertex(i) = Vertex(i, 1000, -1)
 
 # the graph itself contains a list of vertices and edges
 mutable struct Graph
-    nodes :: Array{Vertex,1}
+    vertices :: Array{Vertex,1}
     edges :: Array{Tuple{Int64,Int64},1}
 end
 
 
 function findVertex(g::Graph, id::Int)
-# take a graph and id and returns the vertex with id
-    for n in g.nodes
+    # take a graph and id and returns the vertex with id
+    # not needed, if G.vertices[n] has id = n
+    for n in g.vertices
         if(n.ID == id)
             return n
         end
@@ -38,15 +27,15 @@ function findVertex(g::Graph, id::Int)
 
 function init_single_source(G, s)
     # through the graph creation, all vertices but vertex s have the right attributes
-    n = findVertex(G, s)
+    n = G.vertices[s]
     n.d = 0 # and vertex s gets d = 0
 end
 
 
 function relax(G, u, v, weight_mtx)
     # relax edge (u,v) in graph G with weight matrix weight_mtx
-    uu = findVertex(G, u) # find the corresponding vertices
-    vv = findVertex(G, v)
+    uu = G.vertices[u] # find the corresponding vertices
+    vv = G.vertices[v]
     if(vv.d > uu.d + weight_mtx[u,v]) # make the check if we can relax
         vv.d = uu.d + weight_mtx[u,v] # update the vertex v. Magically, this works (function take parameters by reference?)
         vv.parent = uu.ID
@@ -56,7 +45,7 @@ end
 function bellman_ford(G, s, weight_mtx)
     # Bellman-For algorithm
     init_single_source(G, s)
-    for i = 1:length(G.nodes)
+    for i = 1:length(G.vertices)
         for (u,v) in G.edges
             relax(G, u, v, weight_mtx)
         end
@@ -64,8 +53,8 @@ function bellman_ford(G, s, weight_mtx)
 
     # check for cycles with negative weight -> raise an error
     for (u,v) in G.edges
-        uu = findVertex(G, u)
-        vv = findVertex(G, v)
+        uu = G.vertices[u]
+        vv = G.vertices[v]
         if(vv.d > uu.d + weight_mtx[u,v])
             error("graph has cycles with negative weight")
         end
