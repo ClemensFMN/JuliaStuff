936a3058fe90d62ec62eb4cc8e1741276be23a71
Clemens Novak <clemens@familie-novak.net>
2020-11-09 20:18:06 +0100

    Sieve of Eratosthenes

--- eratosthenes.jl
+++ eratosthenes.jl
@@ -5,17 +5,18 @@ function sieve(N :: Int)
   is_primes[1] = false # one is not a prime - this is required for bootstrapping the loop below
   for i = 1:N
     if(is_primes[i] == true) # is i is a prime, we can prepare to fill all multiples with true
-      # j = i*i - that's the original; but I tihnk, we should start filling the sieve with true from position 2*i onwards...
+      # j = i*i - that's the original; but I think, we should start filling the sieve with true from position 2*i onwards...
       j = 2*i # like this
       if j > N
-        return find(is_primes) # if the first true would exceed the array, we are done and return the indices of the true's
+        return findall(is_primes) # if the first true would exceed the array, we are done and return the indices of the true's
       else
-        is_primes[j:i:N] = false # otherwise we set the sieve positions at multiples of i to false
+        is_primes[j:i:N] .= false # otherwise we set the sieve positions at multiples of i to false
       end
     end
   end
 end
 
 
-res = sieve(100)
+# res = sieve(100)
+res = sieve(1_000_000)
 println(res)
