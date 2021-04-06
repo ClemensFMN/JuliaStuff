5a1b3bea4dbe13eeb70ee4f96110a9fbb0c622a8
Clemens Novak <clemens@familie-novak.net>
2021-04-06 14:55:18 +0200

    count / collect (fermat) pseudoprimes

---
+++ NumberTheory/pseudo_primes.jl
@@ -0,0 +1,25 @@
+# calculate the pseudoprimes of a given composite number n
+function getPseudoPrimes(n)
+    n = BigInt(n)
+    cnt = 0
+    pp = []
+    avec = 1:n
+    for a in avec
+        # calc a^n \mod n
+        a = BigInt(a)        
+        x = mod(a^n, n)        
+        # x = powermod(a,n,n)
+        if(x == a)
+            push!(pp, a)
+            cnt += 1
+        end
+    end
+    # return(cnt, pp)
+    return(cnt)
+end
+
+# pp, cnt = getPseudoPrimes(341)
+
+nvec = 101:2:201
+
+res = getPseudoPrimes.(nvec)
