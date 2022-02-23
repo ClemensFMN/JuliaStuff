41ad07655bd3c24fb244e2c3984a5768a27bc7d2
Clemens Novak <clemens@familie-novak.net>
2022-02-23 15:16:52 +0100

    version update

--- ML/layers_flux.jl
+++ ML/layers_flux.jl
@@ -2,15 +2,23 @@ using Flux
 
 
 
-d1 = Dense(5,2) # creates a dense layer with y = sigma(W * x .+ b)
+d1 = Dense(3,2) # creates a dense layer with y = W * x .+ b
 
-x = randn(5)
+x = randn(3)
 
 res1 = d1(x)
-res2 = d.W * x .+ b # same
+res2 = d1.weight * x .+ d1.bias # same
 
+@show res1, res2
 
+@show params(d1)
 
-#d2 = Chain(Dense(5,2), softmax)
+loss(x) = Flux.Losses.mse(d1(x) , [0.5, 0.5])
 
+grads = gradient(params(d1)) do
+    loss(x)
+end
+for p in params(d1)
+    @show p, grads[p]
+end
 
